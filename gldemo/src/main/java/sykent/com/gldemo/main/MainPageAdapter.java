5e684385a26c85940f0f5034d9996f1df7586cdf
Sykent Lao <1725931880@qq.com>
2019-07-04 10:54:21 +0800

    小改动

--- gldemo/src/main/java/sykent/com/gldemo/main/MainPageAdapter.java
+++ gldemo/src/main/java/sykent/com/gldemo/main/MainPageAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -40,7 +39,7 @@ public class MainPageAdapter extends RecyclerView.Adapter<MainPageAdapter.ViewHo
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.activity_main_view_holder, parent, false);
+                .inflate(R.layout.main_view_holder, parent, false);
         return new ViewHolder(view);
     }
 
